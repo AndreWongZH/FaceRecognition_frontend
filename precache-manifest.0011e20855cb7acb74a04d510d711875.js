f2d5f760c8e28dd01c39ae1925e97fce4449fc50
Andre Wong <wongandre98@gmail.com>
2020-01-08 09:51:32 +0800

    Updates

---
+++ precache-manifest.0011e20855cb7acb74a04d510d711875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04336b30fbd51fca9e28b6f946e2a59f",
+    "url": "/FaceRecognition_frontend/index.html"
+  },
+  {
+    "revision": "21b3be6c47fcb2b7314b",
+    "url": "/FaceRecognition_frontend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "689e811391b35d5d01f3",
+    "url": "/FaceRecognition_frontend/static/css/main.1489f8d0.chunk.css"
+  },
+  {
+    "revision": "21b3be6c47fcb2b7314b",
+    "url": "/FaceRecognition_frontend/static/js/2.c9e1a241.chunk.js"
+  },
+  {
+    "revision": "88dd02bda13cdbcf74e3b472e0a270c8",
+    "url": "/FaceRecognition_frontend/static/js/2.c9e1a241.chunk.js.LICENSE"
+  },
+  {
+    "revision": "689e811391b35d5d01f3",
+    "url": "/FaceRecognition_frontend/static/js/main.dc3019e1.chunk.js"
+  },
+  {
+    "revision": "b4fe06b65c4dd0f534c4",
+    "url": "/FaceRecognition_frontend/static/js/runtime-main.8d424dfb.js"
+  }
+]);
